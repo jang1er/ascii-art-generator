b32fa249a3403211d0a10f72c2f92bb96028cbea
jonas kruger <jonas.kruger@uni-ulm.de>
2025-11-11 17:03:35 +0100

    feat: implemented tests for ascii lib
    added Image tests
    added AveragingScaler tests

--- tests/ascii-lib/ImageScaler/test_ImageScaler.cpp
+++ tests/ascii-lib/ImageScaler/test_ImageScaler.cpp
@@ -1,6 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include "common/Image/Image.hpp"
-#include "common/Image/AveragingImageScaler.hpp"
+#include "common/Image/Scaling/AveragingImageScaler.hpp"
 
 void InitImage(Image &img, size_t width, size_t height, size_t nocc){
     img.width = width;
@@ -94,6 +94,97 @@ TEST_CASE("ImageScaler default test", "[imageScaler]") {
         auto scaledImg = scaler.ScaleImageTo(img, width/2, height/4);
         REQUIRE(scaledImg.width == width/2);
         REQUIRE(scaledImg.height == height/4);
+        REQUIRE(scaledImg.numberOfColorChannels == nocc);
+        REQUIRE(TestImage(scaledImg));
+    }
+
+}
+
+TEST_CASE("ImageScaler non-uniform test", "[imageScaler]") {
+    Image img;
+    size_t width = 1920;
+    size_t height = 1080;
+    size_t nocc = 3;
+
+    // init the image
+    InitImage(img, width, height, nocc);
+
+    // get scaler
+    AveragingScaler scaler;
+
+    SECTION( "scaling to 1/2 uniformly" ){
+        auto scaledImg = scaler.ScaleImageTo(img, width/2, height/2);
+        REQUIRE(scaledImg.width == width/2);
+        REQUIRE(scaledImg.height == height/2);
+        REQUIRE(scaledImg.numberOfColorChannels == nocc);
+        REQUIRE(TestImage(scaledImg));
+    }
+
+    SECTION( "scaling to 1/4 uniformly" ){
+        auto scaledImg = scaler.ScaleImageTo(img, width/4, height/4);
+        REQUIRE(scaledImg.width == width/4);
+        REQUIRE(scaledImg.height == height/4);
+        REQUIRE(scaledImg.numberOfColorChannels == nocc);
+        REQUIRE(TestImage(scaledImg));
+    }
+
+    SECTION( "scaling to 1/8 uniformly" ){
+        auto scaledImg = scaler.ScaleImageTo(img, width/8, height/8);
+        REQUIRE(scaledImg.width == width/8);
+        REQUIRE(scaledImg.height == height/8);
+        REQUIRE(scaledImg.numberOfColorChannels == nocc);
+        REQUIRE(TestImage(scaledImg));
+    }
+
+    SECTION( "scaling to 1/2 width" ){
+        auto scaledImg = scaler.ScaleImageTo(img, width/2, height);
+        REQUIRE(scaledImg.width == width/2);
+        REQUIRE(scaledImg.height == height);
+        REQUIRE(scaledImg.numberOfColorChannels == nocc);
+        REQUIRE(TestImage(scaledImg));
+    }
+
+    SECTION( "scaling to 1/2 height" ){
+        auto scaledImg = scaler.ScaleImageTo(img, width, height/2);
+        REQUIRE(scaledImg.width == width);
+        REQUIRE(scaledImg.height == height/2);
+        REQUIRE(scaledImg.numberOfColorChannels == nocc);
+        REQUIRE(TestImage(scaledImg));
+    }
+
+    SECTION( "scaling to 1/4 width 1/2 height" ){
+        auto scaledImg = scaler.ScaleImageTo(img, width/4, height/2);
+        REQUIRE(scaledImg.width == width/4);
+        REQUIRE(scaledImg.height == height/2);
+        REQUIRE(scaledImg.numberOfColorChannels == nocc);
+        REQUIRE(TestImage(scaledImg));
+    }
+
+    SECTION( "scaling to 1/2 width 1/4 height" ){
+        auto scaledImg = scaler.ScaleImageTo(img, width/2, height/4);
+        REQUIRE(scaledImg.width == width/2);
+        REQUIRE(scaledImg.height == height/4);
+        REQUIRE(scaledImg.numberOfColorChannels == nocc);
+        REQUIRE(TestImage(scaledImg));
+    }
+    SECTION( "scaling to fixed 100 x 100" ){
+        auto scaledImg = scaler.ScaleImageTo(img,100,100);
+        REQUIRE(scaledImg.width == 100);
+        REQUIRE(scaledImg.height == 100);
+        REQUIRE(scaledImg.numberOfColorChannels == nocc);
+        REQUIRE(TestImage(scaledImg));
+    }
+    SECTION( "scaling to fixed 200 x 100" ){
+        auto scaledImg = scaler.ScaleImageTo(img,200,100);
+        REQUIRE(scaledImg.width == 200);
+        REQUIRE(scaledImg.height == 100);
+        REQUIRE(scaledImg.numberOfColorChannels == nocc);
+        REQUIRE(TestImage(scaledImg));
+    }
+    SECTION( "scaling to fixed 640 x 320" ){
+        auto scaledImg = scaler.ScaleImageTo(img,640,320);
+        REQUIRE(scaledImg.width == 640);
+        REQUIRE(scaledImg.height == 320);
         REQUIRE(scaledImg.numberOfColorChannels == nocc);
         REQUIRE(TestImage(scaledImg));
     }
