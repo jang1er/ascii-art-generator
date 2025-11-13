ed0cbb62d50dc7029b5f9caa6a43c1a2014f6f8c
jonas kruger <jonas.kruger@uni-ulm.de>
2025-11-13 13:53:27 +0100

    refactor: fixed spelling mistakes

--- tests/ascii-lib/AsciiGenerator/test_DefaultAsciiGenerator.cpp
+++ tests/ascii-lib/AsciiGenerator/test_DefaultAsciiGenerator.cpp
@@ -8,7 +8,7 @@
 
 TEST_CASE("DefaultAsciiGenerator KingFisher", "[DefaultAsciiGenerator]") {
     
-    std::cout << "TEST:: Loaing kingfisher image" << std::endl;
+    std::cout << "TEST:: Loading kingfisher image" << std::endl;
 
     Image kingfisher("test-images/image-kingfisher.png");
     REQUIRE(kingfisher.data);
@@ -29,17 +29,13 @@ TEST_CASE("DefaultAsciiGenerator KingFisher", "[DefaultAsciiGenerator]") {
 
     DefaultAsciiGenerator generator(scaler);
     std::string ascii = generator.GenerateAscii(kingfisher, context);
-        std::cout << "TEST:: Displaying Ascii" << std::endl;
-
-
+    std::cout << "TEST:: Displaying Ascii" << std::endl;
     std::cout << ascii << std::endl;
-
-
 }
 
 TEST_CASE("DefaultAsciiGenerator Donut 1", "[DefaultAsciiGenerator]") {
     
-    std::cout << "TEST:: Loaing donut image" << std::endl;
+    std::cout << "TEST:: Loading donut image" << std::endl;
 
     Image donut("test-images/image-donut.png");
     REQUIRE(donut.data);
@@ -70,7 +66,7 @@ TEST_CASE("DefaultAsciiGenerator Donut 1", "[DefaultAsciiGenerator]") {
 
 TEST_CASE("DefaultAsciiGenerator Donut 2", "[DefaultAsciiGenerator]") {
     
-    std::cout << "TEST:: Loaing donut 2 image" << std::endl;
+    std::cout << "TEST:: Loading donut 2 image" << std::endl;
 
     Image donut("test-images/image-donut-2.png");
     REQUIRE(donut.data);
